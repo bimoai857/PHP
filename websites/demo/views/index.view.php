443e1d12d2ab9872f11b031513e57a5132fa96ed
Bimochan Shrestha <bimochanshrestha@Bimochans-MacBook-Air.local>
2024-12-06 07:17:56 +0545

    PHP practice

---
+++ websites/demo/views/index.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+
+<body>
+    <div>!!!_________Navbar__________!!!</div>
+    <a href="/about.php">About</a>
+    <a href="/">Home</a>
+    <br/>
+    <div>
+      <div>This is Home Page!!!</div>
+    </div>
+</body>
+
+</html>
