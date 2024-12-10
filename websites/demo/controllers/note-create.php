3bbf67c43a74602a5b2168744603d0cc1b3cd805
Bimochan Shrestha <bimochanshrestha@Bimochans-MacBook-Air.local>
2024-12-10 07:39:38 +0545

    Validation added

--- websites/demo/controllers/note-create.php
+++ websites/demo/controllers/note-create.php
@@ -14,11 +14,25 @@ $description=$_POST['description'];
 $user_id=Constants::USER_ID;
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
-    $data=$db->query('insert into notes(title,description,user_id) values(:title,:description,:user_id)',[
-        
-        'title'=>$title,
-        'description'=>$description,
-        'user_id'=>$user_id
-    ]);
 
+    $errors=[];
+
+    if(strlen($title)==0){
+        $errors['title']="A title is required";
+    }
+    if(strlen($description)==0){
+        $errors['description']="A description is required";
+    }
+ 
+    if(empty($errors)){
+        $data=$db->query('insert into notes(title,description,user_id) values(:title,:description,:user_id)',[
+        
+            'title'=>$title,
+            'description'=>$description,
+            'user_id'=>$user_id
+        ]);
+    
+    }
+
+    require 'views/notes.view.php';
 }
--- websites/demo/views/notes.view.php
+++ websites/demo/views/notes.view.php
@@ -9,24 +9,41 @@ foreach($notes as $index=> $note):
 </ul>
 <?php endforeach; ?>
 <div class="w-full max-w-xs">
+<?php
+        if(isset($errors['title'])){
+          echo "<div class='text-red-500'>".$errors['title']."</div>";
+        }
+        if(isset($errors['description'])){
+          echo "<div class='text-red-500'>".$errors['description']."</div>";
+        }
+      ?>
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"  method="POST" action="/note/create">
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
         Notes
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="note" type="text" placeholder="Enter a note title" name="title">
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="note" type="text" placeholder="Enter a note title" name="title"
+      value="<?php echo isset($_POST['title'])? $_POST['title']:
+      ""
+      ?>">
     </div>
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
         Description
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" type="text" placeholder="Enter a note description" name="description">
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" type="text" placeholder="Enter a note description" name="description"
+      value="<?php echo isset($_POST['description'])? $_POST['description']:
+      ""
+      ?>"
+      >
     </div>
     <div class="flex items-center justify-between">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
         Create
       </button>
+      
     </div>   
+
   </form>
   <p class="text-center text-gray-500 text-xs">
     &copy;2020 Acme Corp. All rights reserved.
